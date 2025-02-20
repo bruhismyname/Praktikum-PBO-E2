b38e915e02a22c132a589a03641ea69120281f74
Rajwaa Muflihul Aufaa <160339796+bruhismyname@users.noreply.github.com>
2025-02-21 02:41:56 +0700

    Update Garis.java

--- Pertemuan 1/Garis.java
+++ Pertemuan 1/Garis.java
@@ -2,6 +2,7 @@
  *  Deskripsi   : berisi atribut dan method dalam class Garis
  *  Pembuat     : Rajwaa Muflihul Aufaa
  *  NIM         : 24060123140189
+ *  Tanggal     : 21 Februari 2025
  */
 
 class Garis {
