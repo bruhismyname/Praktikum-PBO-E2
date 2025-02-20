4f5ff89ed22ca2e2a2ac4cc8866ba9fc3b007845
Rajwaa Muflihul Aufaa <160339796+bruhismyname@users.noreply.github.com>
2025-02-21 01:24:45 +0700

    Add files via upload

---
+++ Pertemuan 1/Garis/Garis.java
@@ -0,0 +1,9 @@
+/*  Nama File   : Garis.java
+ *  Deskripsi   : berisi atribut dan method dalam class Garis
+ *  Pembuat     : Rajwaa Muflihul Aufaa
+ *  NIM         : 24060123140189
+ */
+
+public class Garis{
+    
+}
