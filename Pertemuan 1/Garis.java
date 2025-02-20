5db7321de8f9bc02703f05ae16bd12428c286d49
Rajwaa Muflihul Aufaa <160339796+bruhismyname@users.noreply.github.com>
2025-02-21 02:07:05 +0700

    Add files via upload

---
+++ Pertemuan 1/Garis.java
@@ -0,0 +1,85 @@
+/*  Nama File   : Garis.java
+ *  Deskripsi   : berisi atribut dan method dalam class Garis
+ *  Pembuat     : Rajwaa Muflihul Aufaa
+ *  NIM         : 24060123140189
+ */
+
+class Garis {
+    // ATRIBUT
+    private Titik titikAwal;
+    private Titik titikAkhir;
+    private static int counterGaris = 0;
+
+    // KONSTRUKTOR
+    public Garis() {
+        this.titikAwal = new Titik(0, 0);
+        this.titikAkhir = new Titik(1, 1);
+        counterGaris++;
+    }
+
+    public Garis(Titik titikAwal, Titik titikAkhir) {
+        this.titikAwal = titikAwal;
+        this.titikAkhir = titikAkhir;
+        counterGaris++;
+    }
+
+    // GETTER & SETTER
+    public Titik getTitikAwal() {
+        return titikAwal;
+    }
+
+    public Titik getTitikAkhir() {
+        return titikAkhir;
+    }
+
+    public void setTitikAwal(Titik titikAwal) {
+        this.titikAwal = titikAwal;
+    }
+
+    public void setTitikAkhir(Titik titikAkhir) {
+        this.titikAkhir = titikAkhir;
+    }
+
+    public static int getCounterGaris() {
+        return counterGaris;
+    }
+
+    // METHOD
+    public double getPanjang() {
+        return titikAwal.getJarak(titikAkhir);
+    }
+
+    public double getGradien() {
+        double deltaX = titikAkhir.getAbsis() - titikAwal.getAbsis();
+        double deltaY = titikAkhir.getOrdinat() - titikAwal.getOrdinat();
+        if (deltaX == 0) {
+            throw new ArithmeticException("Gradien tak terdefinisi (garis vertikal)");
+        }
+        return deltaY / deltaX;
+    }
+
+    public Titik getTitikTengah() {
+        double midX = (titikAwal.getAbsis() + titikAkhir.getAbsis()) / 2;
+        double midY = (titikAwal.getOrdinat() + titikAkhir.getOrdinat()) / 2;
+        return new Titik(midX, midY);
+    }
+
+    public boolean isSejajar(Garis g) {
+        return this.getGradien() == g.getGradien();
+    }
+
+    public boolean isTegakLurus(Garis g) {
+        return this.getGradien() * g.getGradien() == -1;
+    }
+
+    public void printGaris() {
+        System.out.println("Garis dari Titik " + titikAwal.getAbsis() + ", " + titikAwal.getOrdinat() +
+                " ke Titik " + titikAkhir.getAbsis() + ", " + titikAkhir.getOrdinat());
+    }
+
+    public String getPersamaanGaris() {
+        double m = getGradien();
+        double c = titikAwal.getOrdinat() - m * titikAwal.getAbsis();
+        return "y = " + m + "x + " + c;
+    }
+}
---
+++ Pertemuan 1/MGaris.java
@@ -0,0 +1,55 @@
+/*  Nama File   : MGaris.java
+ *  Deskripsi   : berisi atribut dan method dalam class Titik
+ *  Pembuat     : Rajwaa Muflihul Aufaa
+ *  NIM         : 24060123140189
+ */
+
+public class MGaris {
+    public static void main(String[] args) {
+        System.out.println("*****************************************************");
+        System.out.println("*   Praktikum Pemrograman Berorientasi Objek E2     *");
+        System.out.println("*          Oleh: Rajwaa Muflihul Aufaa              *");
+        System.out.println("*          INITIALIZATION PROGRAM GARIS             *");
+        System.out.println("*****************************************************");
+        System.out.println("\n/******************* GARIS *******************/");
+        Titik A = new Titik(-2,0);
+        Titik B = new Titik(0,4);
+
+        Garis garis1 = new Garis(); // Konstruktor default
+        System.out.println("Garis 1: ");
+        garis1.printGaris();
+        
+        Garis garis2 = new Garis(A,B);
+        System.out.println("Garis 2: ");
+        garis2.printGaris();
+
+        Garis garis3 = new Garis(new Titik(0,0), new Titik(3,4));
+        System.out.println("Garis 3: ");
+        garis3.printGaris();
+
+        System.out.println("\n/******************* COUNTER GARIS *******************/");
+        System.out.println("Jumlah objek Garis yang telah dibuat: " + Garis.getCounterGaris());
+
+        System.out.println("\n/******************* PANJANG GARIS *******************/");
+        System.out.println("Panjang Garis 2: " + garis2.getPanjang());
+
+        System.out.println("\n/******************* GRADIEN GARIS *******************/");
+        System.out.println("Gradien Garis 2: " + garis2.getGradien());
+
+        System.out.println("\n/******************* TITIK TENGAH *******************/");
+        Titik titikTengah = garis2.getTitikTengah();
+        System.out.println("Titik tengah Garis 2: (" + titikTengah.getAbsis() + ", " + titikTengah.getOrdinat() + ")");
+
+        System.out.println("\n/******************* IS SEJAJAR *******************/");
+        System.out.println("Apakah Garis 2 sejajar dengan Garis 3? " + garis2.isSejajar(garis3));
+
+        System.out.println("\n/******************* IS TEGAK LURUS *******************/");
+        System.out.println("Apakah Garis 2 tegak lurus dengan Garis 3? " + garis2.isTegakLurus(garis3));
+
+        System.out.println("\n/******************* PERSAMAAN GARIS *******************/");
+        System.out.println("Persamaan Garis dari Titik (-2,0) ke (0,4): ");
+        System.out.println(garis2.getPersamaanGaris());
+
+        System.out.println("\n/******************* END PROGRAM GARIS *******************/");
+    }
+}
