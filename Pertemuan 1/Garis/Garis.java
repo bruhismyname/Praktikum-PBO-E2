f62517b3930f160fb0274c681bf0ee598936ae2b
Rajwaa Muflihul Aufaa <160339796+bruhismyname@users.noreply.github.com>
2025-02-21 01:28:18 +0700

    Delete Pertemuan 1 directory

--- Pertemuan 1/Garis/Garis.java
+++
@@ -1,9 +0,0 @@
-/*  Nama File   : Garis.java
- *  Deskripsi   : berisi atribut dan method dalam class Garis
- *  Pembuat     : Rajwaa Muflihul Aufaa
- *  NIM         : 24060123140189
- */
-
-public class Garis{
-    
-}
