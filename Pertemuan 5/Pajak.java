d62abd8e949b085a8bb88a5ed9923e83af6a4a0b
Rajwaa Muflihul Aufaa <160339796+bruhismyname@users.noreply.github.com>
2025-03-23 01:28:22 +0700

    Add files via upload

---
+++ Pertemuan 5/Pajak.java
@@ -0,0 +1,10 @@
+/*  Nama File   : Pajak.java
+ *  Deskripsi   : Interface Pajak merepresentasikan pajak yang harus dibayar oleh suatu objek.
+ *  Pembuat     : Rajwaa Muflihul Aufaa
+ *  NIM         : 24060123140189
+ *  Tanggal     : 20 Maret 2025
+ */
+
+public interface Pajak {
+  double hitungPajak();
+}
