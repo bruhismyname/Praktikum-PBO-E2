9321336b03e4bf1afa051b68bcf4af6ae1717de7
Rajwaa Muflihul Aufaa <160339796+bruhismyname@users.noreply.github.com>
2025-05-26 14:07:39 +0700

    Add files via upload

---
+++ Pertemuan 11/13a-GUI-Collection/src/main/java/com/mycompany/pbogui11a/GUI_Collection_11a.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pbogui11a;
+
+/**
+ *
+ * @author bruhismyname/Rajwaa
+ */
+public class GUI_Collection_11a {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
